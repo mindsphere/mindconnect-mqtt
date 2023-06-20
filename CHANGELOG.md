f4375b1a7d71fce34d999061c75c30a907d774d2
Doehl <christoph.doehl@siemens.com>
2023-06-20 00:40:22 -0700

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Changelog - MindConnect MQTT
+
+## MindConnect MQTT Quick-Start Help 1.0.0
+
+- Initial Version
---
+++ resources/uninstall.sh
@@ -0,0 +1,4 @@
+
+sudo systemctl stop mrs-client
+sudo rm -f /etc/systemd/system/mrs-client.service
+sudo systemctl daemon-reload
